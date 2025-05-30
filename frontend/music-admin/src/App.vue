606d71e8302d1f0a93ce3b44766809817614436b
Dalekink <dctalk12345@gmail.com>
2025-05-30 19:58:32 -0400

    Refactor App.vue to use Vuetify layout and remove unused components

--- frontend/music-admin/src/App.vue
+++ frontend/music-admin/src/App.vue
@@ -1,44 +1,23 @@
-<script setup>
-// import HelloWorld from './components/HelloWorld.vue' // Comentamos o eliminamos el HelloWorld si no se usa
-import UserCreateForm from './components/UserCreateForm.vue';
-</script>
-
 <template>
-  <div id="app">
-    <header>
-      <h1>Gestión de Banda</h1>
-    </header>
-    <main>
-      <UserCreateForm />
-      <!-- <HelloWorld msg="Vite + Vue" /> --> {/* Comentamos o eliminamos el HelloWorld si no se usa */}
-    </main>
-  </div>
+  <v-app>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<style scoped>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 20px;
-}
+<script setup>
+</script>
 
-header h1 {
-  margin-bottom: 30px;
+<style>
+html, body, #app, .v-application, .v-application__wrap {
+  height: 100%;
+  margin: 0;
+  padding: 0;
 }
-
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.v-main {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
 }
 </style>
